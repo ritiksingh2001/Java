213bbcf514f0a403d02004902255b54277859dbe
Ritik Singh <105047117+ritiksingh2001@users.noreply.github.com>
2022-10-15 13:25:45 +0530

    Add files via upload

---
+++ Lab Session/29-09-2022/Parent.java
@@ -0,0 +1,21 @@
+/* 
+ A multilevel inheritance program.
+ program: creating a class having methods  inherited by another class having 
+ its own methods and  all the methods of both the class get called in main  function 
+ Author:Ritik Singh
+ Date:29/09/2022
+ */
+package CoreJava;
+
+ class Parent
+ {   
+	 //creating a default Constructor 
+	  Parent()
+	  { 
+		 System.out.println("Hi! Welcome to Anudeep Foundation ");
+	  }
+	    public void sing()
+	  { 
+		  System.out.println("Twinkle Twinkle.... Little star... :"); 
+      }
+ }
