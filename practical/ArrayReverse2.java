b5398a8b3ee3dac9e08dc2703d9ca6e772263125
Ritik Singh <105047117+ritiksingh2001@users.noreply.github.com>
2022-10-21 20:19:09 +0530

    Add files via upload

---
+++ practical/ArrayReverse2.java
@@ -0,0 +1,23 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class ArrayReverse2 {
+
+	public static void main(String[] args) {
+	Scanner sc= new Scanner (System.in);
+	System.out.println("enter the size of array");
+	int n=sc.nextInt();
+	System.out.println("enter the array elements ");
+	int arr[]=new int[n];
+	for(int i=0;i<n;i++)
+	{
+		arr[i]=sc.nextInt();
+	}
+         System.out.println("reverse of a array");
+         for(int j=n;j>0;j--)
+         {
+        	 System.err.print(arr[j-1]+" ");
+         }
+	}
+}
