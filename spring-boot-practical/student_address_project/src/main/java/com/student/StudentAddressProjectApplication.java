8b12dcae0ebd11e0227a512a0fca24c662fc1f22
Ritik Singh <105047117+ritiksingh2001@users.noreply.github.com>
2022-12-10 18:40:49 +0530

    Add files via upload

---
+++ spring-boot-practical/student_address_project/src/main/java/com/student/StudentAddressProjectApplication.java
@@ -0,0 +1,14 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAddressProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAddressProjectApplication.class, args);
+		System.out.println("student application is running...");
+	}
+
+}
